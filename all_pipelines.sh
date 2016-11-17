95015f57f28b7690ac36aaafac98352b5af16889
Paolo Avesani <avesani@fbk.eu>
2016-11-17 12:05:32 -0500

    Fixed script all_pipeline.sh

--- all_pipelines.sh
+++ all_pipelines.sh
@@ -13,14 +13,16 @@ root=$3
 
 cd /N/dc2/projects/o3d/code
 
-python init_connectome_tract.py $dataset $subject $root &
-python init_preprocessing.py $dataset $subject $root &
-python init_recon_models.py $dataset $subject $root &
-python init_tracking_csddet_trk.py $dataset $subject $root &
-python init_tracking_csdprob_trk.py $dataset $subject $root &
-python init_tracking_dtidet_trk.py $dataset $subject $root &
-wait
+JOB1=`python init_connectome_tract.py $dataset $subject $root &`
+JOB2=`python init_preprocessing.py $dataset $subject $root &`
+JOB3=`python init_recon_models.py $dataset $subject $root &`
+JOB4=`python init_tracking_csddet_trk.py $dataset $subject $root &`
+JOB5=`python init_tracking_csdprob_trk.py $dataset $subject $root &`
+JOB6=`python init_tracking_dtidet_trk.py $dataset $subject $root &`
+JOB7=`python init_dissection_afq_trk.py $dataset $subject $root &`
+wait $JOB1 $JOB2 $JOB3 $JOB4 $JOB5 $JOB6 
 python init_tracking_csddet_tck.py $dataset $subject $root &
 python init_tracking_csdprob_tck.py $dataset $subject $root &
 python init_tracking_dtidet_tck.py $dataset $subject $root &
-wait
+python init_dissection_afq_trk.py $dataset $subject $root &
+
