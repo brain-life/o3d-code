372de0e695727791b3cb1c87869f51e529f19a3b
Paolo Avesani <avesani@fbk.eu>
2016-11-19 20:46:10 -0500

    Added nginx and rsync docker config

---
+++ server/nginx/run.sh
@@ -0,0 +1,11 @@
+
+#you need to mount /mnt/soichifs as nginx so that nginx can access it (sshfs limiation?)
+docker rm -f nginx
+docker run --name nginx \
+	-v /root/dc2:/usr/share/nginx/html \
+	-v `pwd`/nginx.conf:/etc/nginx/nginx.conf \
+	-p 0.0.0.0:80:80 \
+	-d nginx
+#-p 0.0.0.0:443:443 \
+#-v /root/dc2karst/Downloads/dc2git/git-dc2karst:/usr/share/nginx/html \
+#-v `pwd`/html:/usr/share/nginx/html \
---
+++ server/rsync/run.sh
@@ -0,0 +1,9 @@
+docker rm -f rsync
+docker run \
+    --name rsync \
+    --restart=always \
+    -p 0.0.0.0:1873:873 \
+    -v /root/dc2:/dc2 \
+    -v `pwd`/rsyncd.conf:/etc/rsyncd.conf \
+    -d bfosberry/rsync
+
