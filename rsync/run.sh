8bf93854aae5f5fc27b5657ce92cfee47358c254
Soichi Hayashi <hayashis@iu.edu>
2017-06-23 10:03:41 -0400

    Update run.sh
    We don't need to specify 0.0.0.0 as it is default. security group is placed by openstack to give access to the port.
    
    On the VM, the run.sh is currently configured to use port 873 not 1873.

--- rsync/run.sh
+++ rsync/run.sh
@@ -2,7 +2,7 @@ docker rm -f rsync
 docker run \
     --name rsync \
     --restart=always \
-    -p 0.0.0.0:1873:873 \
+    -p 1873:873 \
     -v /root/dc2:/dc2 \
     -v `pwd`/rsyncd.conf:/etc/rsyncd.conf \
     -d bfosberry/rsync
