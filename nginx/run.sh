9258517caf4f4a5800c61900855d3c8d4b7cae4f
Soichi Hayashi <hayashis@iu.edu>
2017-06-23 10:01:56 -0400

    Update run.sh
    we don't need to specify 0.0.0.0 as it is default. security group is placed by openstack to give access to port 80

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,7 +4,7 @@ docker rm -f nginx
 docker run --name nginx \
 	-v /root/o3d/data:/usr/share/nginx/html \
 	-v `pwd`/nginx.conf:/etc/nginx/nginx.conf \
-	-p 0.0.0.0:80:80 \
+	-p 80:80 \
 	-d nginx
 #-p 0.0.0.0:443:443 \
 #-v /root/dc2karst/Downloads/dc2git/git-dc2karst:/usr/share/nginx/html \
